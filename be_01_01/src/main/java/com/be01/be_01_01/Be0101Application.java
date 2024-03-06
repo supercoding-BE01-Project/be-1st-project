7874d40cee04c88be12464be7f1dae94c5aa0175
Amsoboro <ndsldog@naver.com>
2024-03-06 21:53:48 +0900

    be01팀01차 - JDK17 - 롬복, 타임리프, h2, jpa 라이브러리추가

---
+++ be_01_01/src/main/java/com/be01/be_01_01/Be0101Application.java
@@ -0,0 +1,13 @@
+package com.be01.be_01_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Be0101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Be0101Application.class, args);
+	}
+
+}
---
+++ be_01_01/src/test/java/com/be01/be_01_01/Be0101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be01.be_01_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Be0101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
