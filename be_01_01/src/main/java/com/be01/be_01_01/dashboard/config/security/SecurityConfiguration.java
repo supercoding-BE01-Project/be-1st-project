6712eaca16f417761de9f737e42fe43394bd7fe6
carter-sw <6k2hbc@gmail.com>
2024-03-14 22:34:33 +0900

    Revert "3.14"
    This reverts commit a74b6d658f97a5a497b14dd2391dfd542a690e24.

--- be_01_01/src/main/java/com/be01/be_01_01/dashboard/config/security/SecurityConfiguration.java
+++ be_01_01/src/main/java/com/be01/be_01_01/dashboard/config/security/SecurityConfiguration.java
@@ -59,7 +59,8 @@ public class SecurityConfiguration {
                 .authorizeRequests()
                 // 요청에 대한 접근 권한 설정
                 .requestMatchers("/resources/static/**", "/api/*/*").permitAll() // 정적 자원 및 특정 API 경로는 모두에게 허용
-                .requestMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/security", "/swagger-ui.html", "/swagger-ui/**", "/webjars/**", "/swagger/**").permitAll()
+                .requestMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/security", "/swagger-ui/**", "/webjars/**","/swagger/**").permitAll() // Swagger 문서 관련 경로는 모두에게 허용
+                .anyRequest().authenticated() // 그 외 모든 요청은 인증을 필요로 함
                 .anyRequest().authenticated() // 그 외 모든 요청은 인증을 필요로 함
                 .and()
                 .exceptionHandling()
